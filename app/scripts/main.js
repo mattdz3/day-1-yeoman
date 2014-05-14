8e53b37f9a05c5e3666a1ea2b573af91bcc6fb85
Matt Dougherty <mattz325@gmail.com>
2014-05-13 21:17:40 -0400

    added some examples

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,12 +12,50 @@ $('.button-dbl').dblclick(function() {
  
 
 $('.button-fade').click(function() {
-  $('.fade').fadeOut( "slow", function() {
-  });
+	$('.fade').fadeOut('slow', function() {
+	});
 });
 
 
 $('.button-slideup').click(function() {
-  $('.slideup').slideUp( "slow", function() {
-  });
+	$('.slideup').slideUp('slow', function() {
+	});
 });
+
+
+$('.button-slidedown').click(function() {
+	$('.slidedown').slideDown('slow', function() {
+	});
+});
+
+
+$('.button-toggle').click(function() {
+	$('.toggle').toggle('fast', function() {
+	});
+});
+
+var para = $('.button-detach').click(function() {
+  if (para) {
+    para.appendTo('detach');
+    para = null;
+  } else {
+    para = $('.para').detach();
+  }
+});
+
+
+var first, last;
+
+$('.button-timestamp').click(function(time) {
+	if (first) {
+    	last = time.timeStamp - first;
+    $('.timestamp').append("It's been " + last + " milliseconds!  ");
+}
+	first = time.timeStamp;
+});
+
+$('.button-event-type').click(function(click) {
+	alert(click.type);
+});
+
+
